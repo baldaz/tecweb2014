409386e637cb0f160ce45f153c0de8ab9d3c4668
baldaz <a.g.baldan@gmail.com>
2014-04-19 15:42:05 +0200

    jquery realtime form

--- public_html/js/rtc_form.js
+++ public_html/js/rtc_form.js
@@ -1,13 +1,30 @@
-var triggered=0;
 $("#telefono").keydown(function(event){
     if((event.which < 47 || event.which > 57) && event.which !=8 && event.which != 9 && event.which != 46){
+	event.preventDefault();
 	$("#err_telefono").text("Inserire numeri");
-	triggered++;
     }
-    else if(event.which == 46 || event.which == 8 || event.which==9){
-	triggered--;
-	if(triggered==0){
-	    $("#err_telefono").text("");
-	}
+    else{
+	$("#err_telefono").text("");
     }
 });
+
+/*
+$("#telefono").keydown(function(event) {
+  // Backspace, tab, enter, end, home, left, right,decimal(.)in number part, decimal(.) in alphabet
+  // We don't support the del key in Opera because del == . == 46.
+  var controlKeys = [8, 9, 13, 35, 36, 37, 39,110,190];
+  // IE doesn't support indexOf
+  var isControlKey = controlKeys.join(",").match(new RegExp(event.which));
+  // Some browsers just don't raise events for control keys. Easy.
+  // e.g. Safari backspace.
+  if (!event.which || // Control keys in most browsers. e.g. Firefox tab is 0
+      (49 <= event.which && event.which <= 57) || // Always 1 through 9
+      (96 <= event.which && event.which <= 106) || // Always 1 through 9 from number section 
+      (48 == event.which && $(this).attr("value")) || // No 0 first digit
+      (96 == event.which && $(this).attr("value")) || // No 0 first digit from number section
+      isControlKey) { // Opera assigns values for control keys.
+    return;
+  } else {
+    event.preventDefault();
+  }
+});*/
